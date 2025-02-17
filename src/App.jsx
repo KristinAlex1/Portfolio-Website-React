b75b216f1aa32b2f7ede09ed8d96f369fa05cd95
KristinAlex1 <kristinalex2001@gmail.com>
2025-02-17 19:45:36 -0400

    Added logo

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ const App = () => {
   return (
     <div className="relative h-screen w-full bg-neutral-950">
       {/* ✅ Background Gradient Fix */}
-      <div className="absolute top-0 left-0 w-full h-full z-[-1] bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      <div className="absolute inset-0 z-[-1] h-screen w-screen bg-neutral-950 bg-custom-gradient"></div>
+
+
 
       {/* Navbar */}
       <Navbar />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,45 +9,45 @@ const Navbar = () => {
      <div className="flex justify-between items-center px-10 py-5">
       {/* Logo Section */}
       <div>
-        <img className="h-[6rem]" src={logo} alt="logo" />
+        <img className="h-[10rem] w-[12rem]" src={logo} alt="logo" />
       </div>
 
       {/* Social Icons */}
-      <div className="flex justify-center items-center gap-5 text-4xl">
+      <div className="flex justify-center items-center gap-5 text-4xl px-10 py-5">
         {/* LinkedIn */}
         <a
           href="https://www.linkedin.com/in/kristin-alex-740a72153"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <FaLinkedin className="text-purple-500 hover:text-purple-700 transition duration-300" />
+          <FaLinkedin className="text-gray-500 hover:text-blue-700 transition duration-300" />
         </a>
 
         {/* GitHub */}
         <a
-          href="https://github.com/YOUR_GITHUB_USERNAME"
+          href="https://github.com/KristinAlex1"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <FaGithub className="text-purple-500 hover:text-purple-700 transition duration-300" />
+          <FaGithub className="text-gray-500 hover:text-gray-100 transition duration-300" />
         </a>
 
         {/* Instagram */}
         <a
-          href="https://instagram.com/YOUR_INSTAGRAM_USERNAME"
+          href="https://www.instagram.com/kristin_alex1/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <FaInstagram className="text-purple-500 hover:text-purple-700 transition duration-300" />
+          <FaInstagram className="text-gray-500 hover:text-purple-700 transition duration-300" />
         </a>
 
         {/* Twitter */}
         <a
-          href="https://twitter.com/YOUR_TWITTER_USERNAME"
+          href="https://x.com/KristinAlex5"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <FaSquareXTwitter className="text-purple-500 hover:text-purple-700 transition duration-300" />
+          <FaSquareXTwitter className="text-gray-500 hover:text-gray-900 transition duration-300" />
         </a>
       </div>
     </div>
