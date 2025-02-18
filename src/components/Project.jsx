52a817bb30d7e26ccd84d7c793899ff738d858aa
KristinAlex1 <kristinalex2001@gmail.com>
2025-02-18 17:50:02 -0400

    Aadded Project section

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import { PROJECTS } from '../constants'
 
 const Project = () => {
   return (
     <div className='border-b border-neutral-900 pb-4'>
-        <h2 className='my-20 text-center text-4xl'>Projects</h2>
+        <h2 className='my-20 text-center font-thin text-6xl'>Projects</h2>
+        <div>{PROJECTS.map((project, index) =>(
+            <div key={index} className='mb-4 mt-[1rem] flex flex-wrap lg:justify-center'>
+                <div>
+                    <img 
+                    src={project.image} 
+                    alt={project.title}
+                    width={150}
+                    height={100}
+                    className='mb-6 rounded'
+                     />
+
+                </div>
+                <div className='w-full mt-[5rem] ml-[4rem] max-w-xl lg:w-3/4'>
+                    <h6 className='mb-2 text-2xl font-semibold'>{project.title}</h6>
+                    <p className='mb-4 text-xl text-gray-500'>{project.description}</p>
+                    {project.technologies.map((tech,index) => (
+                        <span className='mr-2 text-2xl rounded bg-neutral-900 px-2 py-1 text-sssm font-medium text-purple-900' key={index}>
+                            {tech}
+                        </span>
+                    ))}
+                    <h6 className='mt-[2rem] text-xl'>Link: <a className='text-b;ue-300 hover:text-blue-500' href={`${project.link}`}>{project.link} </a></h6>
+
+                </div>
+                
+                
+            </div>
+
+        ))}</div>
     
     </div>
   )
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,5 @@
 import project1 from "../assets/Netflix.png"
+import project2 from "../assets/project2.png"
 
 export const HERO_CONTENT = `I am a passionate full-stack developer with a keen interest in building dynamic and scalable web applications. With hands-on experience in React, React-Native, Javascript, Redux, Firebase, and Tailwind CSS for front-end development, and Node.js and Stripe for seamless back-end integration, I specialize in creating intuitive user interfaces and robust functionalities. My goal is to leverage my expertise in modern web technologies to develop innovative solutions that enhance user experience and drive digital transformation.`;
 
@@ -12,14 +13,23 @@ export const PROJECTS = [
     title: "Netflix Clone",
     image: project1,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+      "Netflix Clone – A full-stack streaming platform built with React, Redux, Firebase, Tailwind CSS, and Stripe. It features secure authentication, movie browsing, subscription plans with Stripe payments, and a fully responsive UI. Users can sign in, explore content, and subscribe to premium plans. Deployed with Firebase Hosting.",
+    technologies: ["React", "Tailwind", "Firebase", "Redux"],
+    link: "https://netflix-clone-b312c.web.app/"
+  },
+  {
+    title: "Currency Calculator",
+    image: project2,
+    description:
+      "A React Native app for converting CAD to multiple currencies. Features a FlatList for easy currency selection, real-time calculations, and user-friendly design. Perfect for quick and accurate conversions!",
+    technologies: ["React-Native", "Typescript", "CSS"],
+    link: "https://github.com/KristinAlex1/Currency-Calculator-React-Navtive-App"
   },
   
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "35 Crowell Dr, Wolfville, NS , Canada",
+  phoneNo: "+1 343 989 6022",
+  email: "kristinalex2001@gmail.com",
 };
